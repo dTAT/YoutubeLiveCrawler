fa0fa7a14a1b49f6f2eaf50dd9348ca0d6f1b8a7
Tatsuya FUJIWARA <fjtatsuya@gmail.com>
2018-02-02 16:09:22 +0900

    feature: キューイングして遅延させたメッセージを表示するように変更

--- unity/YoutubeCommentCrawler/Assets/Scripts/CommentQueue.cs
+++ unity/YoutubeCommentCrawler/Assets/Scripts/CommentQueue.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.TestTools;
 using WebComment;
 
 public class CommentQueue : MonoBehaviour {
+
+	[SerializeField]
+	float dequeueDelay = 1.0f;
+	Queue<CommentChunk> commentQueue;
+	void Awake () {
+		commentQueue = new Queue<CommentChunk> ();
+	}
+	void Start () {
+		StartCoroutine (ProcessDequeue ());
+	}
+
+	IEnumerator ProcessDequeue () {
+		while (true) {
+			if (commentQueue.Count > 0) {
+				var c = commentQueue.Dequeue ();
+				PassToDrawer (c);
+			}
+			yield return new WaitForSeconds (dequeueDelay);
+		}
+	}
 	CommentDrawer drawer;
 	public void SetDrawer (CommentDrawer d) {
 		drawer = d;
 	}
 	public void AddCommentRange (IList<CommentChunk> chunks) {
 		foreach (var c in chunks) {
-			PassToDrawer (c);
+			//PassToDrawer (c);
+			commentQueue.Enqueue (c);
 		}
 	}
 	void PassToDrawer (CommentChunk chunk) {
+		//ßDebug.Log ("pass drawer");
 		drawer.DrawComment (chunk);
 	}
 }
